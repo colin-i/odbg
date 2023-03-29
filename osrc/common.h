edda90cecc11c4b166fa05521efbfee92df5897f
bc <costin.botescu@gmail.com>
2023-03-29 15:08:47 +0300

    chdir,realpath

--- osrc/common.h
+++ osrc/common.h
@@ -3,4 +3,5 @@ const SEEK_SET=0
 
 const dwordstr=10
 
+const asciinul=0
 const asciiminus=0x2D
--- osrc/init3.s
+++ osrc/init3.s
@@ -2,6 +2,7 @@
 format elfobj64
 
 include "../include/common.h"
+include "common.h"
 
 einclude  "/usr/include/ocompiler/log.h"
 
@@ -9,6 +10,8 @@ function files()
 	value pointer=0
 	return #pointer
 endfunction
+
+importx "free" free
 
 #-1 err
 function memorize_program(ss mem,sd end)
@@ -21,10 +24,10 @@ function memorize_program(ss mem,sd end)
 
 	#store for main iteration
 	ss cursor;set cursor mem
+	ss pointer
 
 	while mem!=end
 		importx "strstr" strstr
-		sd pointer
 		setcall pointer strstr(mem,#term)
 		if pointer==(NULL)
 			return -1
@@ -43,11 +46,12 @@ function memorize_program(ss mem,sd end)
 		while cursor!=end
 			setcall pointer strstr(cursor,#term)
 			#if pointer==(NULL) was checked already
-			#log_pathfolder
-			#log_pathname
-			#log_fileend
-			#log_fileend_old
-			#log_line
+			set pointer# (asciinul)
+			sd ret;setcall ret memorize_line(cursor)
+			if ret==-1
+				call free(p#)
+				return -1
+			endif
 			add pointer t_size
 			set cursor pointer
 		endwhile
@@ -55,4 +59,27 @@ function memorize_program(ss mem,sd end)
 	endif
 
 	return -1
+endfunction
+#same
+function memorize_line(ss cursor)
+	#log_line
+	if cursor#==(log_pathname)
+		inc cursor
+		importx "realpath" realpath
+		sd n;setcall n realpath(cursor,(NULL))
+		if n!=(NULL)
+			call free(n)
+		else
+			return -1
+		endelse
+	#log_fileend
+	#log_fileend_old
+	elseif cursor#==(log_pathfolder)
+		inc cursor
+		importx "chdir" chdir
+		sd ret
+		setcall ret chdir(cursor)
+		return ret
+	endelseif
+	return 0
 endfunction
