a2277b25a95b8d11b0d20085a793c8c3be6ad253
sync <costin.b.84@gmail.com>
2025-05-04 05:47:48 +0300

    "sync"

--- osrc/a.c
+++ osrc/a.c
@@ -6,21 +6,21 @@
 
 int main(int argc,char**argv){
 	char*n=argv[1];
-	FILE*pre=fopen(n,"rb");if(pre==NULL)exit(1);
+	FILE*pre=fopen(n,"rb");if(pre==NULL)exit(EXIT_FAILURE);
 	char*bf=NULL;ssize_t sz;
-	sz=getline(&bf,&sz,pre);if(sz==-1)exit(1);
+	sz=getline(&bf,&sz,pre);if(sz==-1)exit(EXIT_FAILURE);
 	bf[sz-1]='\0';
-	long p=ftell(pre);if(p==-1)exit(1);
+	long p=ftell(pre);if(p==-1)exit(EXIT_FAILURE);
 	fseek(pre,0,SEEK_END);
-	long p2=ftell(pre);if(p2==-1)exit(1);
+	long p2=ftell(pre);if(p2==-1)exit(EXIT_FAILURE);
 	fseek(pre,p,SEEK_SET);
 	p2-=p;
-	char*mem=malloc(p2);if(mem==NULL)exit(1);
+	char*mem=malloc(p2);if(mem==NULL)exit(EXIT_FAILURE);
 	fread(mem,p2,1,pre);
 	fclose(pre);
 
 	char*tfile="tempfile.c";
-	FILE*f=fopen(tfile,"wb");if(f==NULL)exit(1);
+	FILE*f=fopen(tfile,"wb");if(f==NULL)exit(EXIT_FAILURE);
 
 	int whatout=fprintf(f,R"(
 #include <%s>
@@ -30,7 +30,7 @@ int main(int argc,char**argv){
 void main(){
 char*file="%s";
 FILE*f=fopen(file,"wb");
-)",bf,argv[2]);if(whatout<0)exit(1);
+)",bf,argv[2]);if(whatout<0)exit(EXIT_FAILURE);
 	free(bf);
 
 	char*s=mem;char*ante;
@@ -41,9 +41,9 @@ FILE*f=fopen(file,"wb");
 			int j=i-1;
 			int show_size=mem[j]==',';
 			if(show_size)mem[j]='\0';
-			whatout=fprintf(f,"fprintf(f,\"const %s_%s=%%lu\\n\",offsetof(%s, %s));\n",ante,s,ante,s);if(whatout<0)exit(1);
+			whatout=fprintf(f,"fprintf(f,\"const %s_%s=%%lu\\n\",offsetof(%s, %s));\n",ante,s,ante,s);if(whatout<0)exit(EXIT_FAILURE);
 			if(show_size){
-				whatout=fprintf(f,"fprintf(f,\"const %s_%s_size=%%lu\\n\",member_size(%s, %s));\n",ante,s,ante,s);if(whatout<0)exit(1);
+				whatout=fprintf(f,"fprintf(f,\"const %s_%s_size=%%lu\\n\",member_size(%s, %s));\n",ante,s,ante,s);if(whatout<0)exit(EXIT_FAILURE);
 			}
 		}
 		s=&mem[i+1];
@@ -51,14 +51,14 @@ FILE*f=fopen(file,"wb");
 	free(mem);
 
 	whatout=fprintf(f,R"(fclose(f);
-})");if(whatout<0)exit(1);
+})");if(whatout<0)exit(EXIT_FAILURE);
 
 	fclose(f);
-	int ws=system("cc tempfile.c");if(WIFEXITED(ws))if(WEXITSTATUS(ws)!=0)exit(1);
+	int ws=system("cc tempfile.c");if(WIFEXITED(ws))if(WEXITSTATUS(ws)!=0)exit(EXIT_FAILURE);
 	unlink(tfile);
 	char*a="./a.out";
 //	int offset=WEXITSTATUS(system(a));
-	ws=system(a);if(WIFEXITED(ws))if(WEXITSTATUS(ws)!=0)exit(1);
+	ws=system(a);if(WIFEXITED(ws))if(WEXITSTATUS(ws)!=0)exit(EXIT_FAILURE);
 	unlink(a);
-	return 0;
+	return EXIT_SUCCESS;
 }
